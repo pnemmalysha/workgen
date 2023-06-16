6039ee0b701b6bb7566bb197ec3ea72cfe7a470c
pnemmalysha <pnemmalysha@gmail.com>
2023-06-16 20:17:40 +0300

    laying out step

--- main_new_boxes.py
+++ main_new_boxes.py
@@ -11,12 +11,50 @@ from kivy.uix.button import Button
 
 
 
+class LeftCol_Left(BoxLayout):
+    def __init__(self, **kwargs):
+        super(LeftCol_Left, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+
+        # check what attributes is
+        print(self.__dir__())
+        for i in self.__dir__():
+            print(i, self.__getattribute__(i))
+
+        self.add_widget(Button(text = "LeftCol_Left_1"))
+        self.add_widget(Button(text = "LeftCol_Left_2"))
+        self.add_widget(Button(text = "LeftCol_Left_3"))
+        self.add_widget(Button(text = "LeftCol_Left_4"))
+        self.add_widget(Button(text = "LeftCol_Left_5"))
+        self.add_widget(Button(text = "LeftCol_Left_6"))
+        self.add_widget(Button(text = "LeftCol_Left_7"))
+        self.add_widget(Button(text = "LeftCol_Left_8"))
+        self.add_widget(Button(text = "LeftCol_Left_9"))
+
+
+class LeftCol_Right(BoxLayout):
+    def __init__(self, **kwargs):
+        super(LeftCol_Right, self).__init__(**kwargs)
+        self.orientation = 'vertical'
+
+        self.add_widget(Button(text = "LeftCol_Right_1"))
+        self.add_widget(Button(text = "LeftCol_Right_2"))
+        self.add_widget(Button(text = "LeftCol_Right_3"))
+        self.add_widget(Button(text = "LeftCol_Right_4"))
+        self.add_widget(Button(text = "LeftCol_Right_5"))
+        self.add_widget(Button(text = "LeftCol_Right_6"))
+        self.add_widget(Button(text = "LeftCol_Right_7"))
+        self.add_widget(Button(text = "LeftCol_Right_8"))
+        self.add_widget(Button(text = "LeftCol_Right_9"))
+
+
+
 class LeftCol(BoxLayout):
     def __init__(self, **kwargs):
         super(LeftCol, self).__init__(**kwargs)
 
-        self.add_widget(Button(text = "LeftCol_Left"))
-        self.add_widget(Button(text = "LeftCol_Right"))
+        self.add_widget(LeftCol_Left(size_hint=(.5, 1)))
+        self.add_widget(LeftCol_Right())
 
 
 class RigthCol(BoxLayout):
